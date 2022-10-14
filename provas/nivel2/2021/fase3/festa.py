78a7229391eb3fd5c455242110c57ade388dcdf5
Robertoskb <97955337+Robertoskb@users.noreply.github.com>
2022-10-13 21:29:42 -0300

    Update festa.py

--- provas/nivel2/2021/fase3/festa.py
+++ provas/nivel2/2021/fase3/festa.py
@@ -1,18 +1,18 @@
 # https://olimpiada.ic.unicamp.br/pratique/p2/2021/f3/festa/
 
 def sortear(it, n):
-    for c, i in enumerate(it):
-        if (c+1) % n == 0:
+    for c, i in enumerate(it, 1):
+        if c % n == 0:
             continue
         yield i
 
 
 N = int(input())
 M = int(input())
-suditos = range(N)
+suditos = range(1, N+1)
 
 for _ in range(M):
     suditos = sortear(suditos, int(input()))
 
 for s, _ in zip(suditos, range(10000)):
-    print(s+1)
+    print(s)
