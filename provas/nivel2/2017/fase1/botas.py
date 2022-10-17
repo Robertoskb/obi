360a3746ae01937f6e06ff6f0e00ebc4abd53257
crowkj <vinicius7pablo@gmail.com>
2022-10-17 17:26:22 -0300

    Add files

---
+++ provas/nivel2/2017/fase1/botas.py
@@ -0,0 +1,13 @@
+n = int(input())
+botas = []
+
+for i in range(n):
+    num, pe = input().split()
+    botas.append([int(num), pe])
+    
+count = 0
+for i in range(30, 61):
+    d, e = botas.count([i, 'D']), botas.count([i, 'E'])
+    count += min(d, e)
+    
+print(count)
