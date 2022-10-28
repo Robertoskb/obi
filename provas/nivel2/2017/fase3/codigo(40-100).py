f5b1221171329e93a35a2617e249176552ba0518
Robertoskb <97955337+Robertoskb@users.noreply.github.com>
2022-10-28 15:32:40 -0300

    Add files

---
+++ provas/nivel2/2017/fase3/codigo(40-100).py
@@ -0,0 +1,21 @@
+# https://olimpiada.ic.unicamp.br/pratique/p2/2017/f3/codigo/
+
+N = int(input())
+
+cadeias = []
+
+resp = ''
+for i in range(N):
+    if not resp:
+        cadeia = input()
+        if cadeia in cadeias:
+            resp = cadeia
+        for j in range(i - 1):
+            for k in range(j + 1, i):
+                if cadeia in cadeias[j] + cadeias[k] or cadeia in cadeias[k] + cadeias[j]:
+                    resp = cadeia
+        cadeias.append(cadeia)
+    else:
+        input()
+
+print(resp or 'ok')
