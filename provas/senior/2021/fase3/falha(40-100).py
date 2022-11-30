b96fe69cc4dfde1726d277eaa7ed4093ce4e1ae5
Robertoskb <joseroberto.tibau.1@gmail.com>
2022-11-30 06:58:11 -0300

    Add file

---
+++ provas/senior/2021/fase3/falha(40-100).py
@@ -0,0 +1,18 @@
+# https://olimpiada.ic.unicamp.br/pratique/ps/2021/f3/falha/
+
+n = int(input())
+
+senhas = sorted((input() for _ in range(n)), key=len, reverse=True)
+
+cont = 0
+for i in range(n-1):
+    maior = senhas[i]
+    for j in range(i+1, n):
+        menor = senhas[j]
+
+        if maior == menor:
+            cont += 2
+        elif menor in maior:
+            cont += 1
+
+print(cont)
