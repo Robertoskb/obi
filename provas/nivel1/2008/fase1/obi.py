c5999e0c7d57c23eb29324ce96a8aeb5fd957fe4
Andreas435 <96071027+Andreas435@users.noreply.github.com>
2024-10-12 19:27:39 -0300

    Update obi.py

--- provas/nivel1/2008/fase1/obi.py
+++ provas/nivel1/2008/fase1/obi.py
@@ -4,8 +4,8 @@ N, P = [int(x) for x in input().split()]
 total = 0
 
 while N:
-  if sum([int(x) for x in input().split()]) >= P:
-    total += 1
-  N -= 1
+    if sum([int(x) for x in input().split()]) >= P:
+        total += 1
+    N -= 1
 
 print(total)
